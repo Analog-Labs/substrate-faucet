f456e8218eea8cbb0df40c0be0212bda1ca5d7b7
Florian Franzen <Florian.Franzen@gmail.com>
2024-04-26 03:00:28 +0200

    env: correct testnet token symbol

--- src/config/env/env.js
+++ src/config/env/env.js
@@ -8,7 +8,7 @@ module.exports = {
     // db: "mongodb://localhost:27017/discord",
     token: process.env.DISCORD_TOKEN,
     prefix: '/',
-    symbol: 'ANALOG',
+    symbol: 'TANLOG',
     decimals: 12,
     ws: process.env.RPC_URL, // url for staging env
     address_type: parseInt(process.env.ADDRESS_TYPE || '51'), // https://github.com/paritytech/substrate/blob/e232d78dd5bafa3bbaae9ac9db08f99e238392db/primitives/core/src/crypto.rs#L444
