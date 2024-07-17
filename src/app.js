9b9d28fd5942e76191ad0fc736653abe25fdc672
ana-junius <junius@analog.one>
2024-07-17 12:18:51 +0800

    update

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ client.on("ready", () => {
     // Example of changing the bot's playing game to something useful. `client.user` is what the
     // docs refer to as the "ClientUser".
     //client.user.setActivity(`...`, { type: 'WATCHING' });
-    
 });
 
 client.on("message", async message => {
